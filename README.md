dfdfcc04c49431f5af13a53ffefcaa75bc3ee56f
feuyeux <feuyeux@gmail.com>
2024-10-05 23:22:10 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # hello-vector-api
+
+The JEP History of Vector API
+
+1. [JEP 338: Vector API (Incubator)](https://openjdk.org/jeps/338) JDK 16
+2. [JEP 414: Vector API (Second Incubator) ](https://openjdk.org/jeps/414)JDK 17
+3. [JEP 417: Vector API (Third Incubator)](https://openjdk.org/jeps/417) JDK 18
+4. [JEP 426: Vector API (Fourth Incubator)](https://openjdk.org/jeps/426) JDK 19
+5. [JEP 438: Vector API (Fifth Incubator)](https://openjdk.org/jeps/438) JDK 20
+6. [JEP 448: Vector API (Sixth Incubator)](https://openjdk.org/jeps/448) JDK 21
+7. [JEP 460: Vector API (Seventh Incubator)](https://openjdk.org/jeps/460) JDK 22
+8. [JEP 469: Vector API (Eighth Incubator)](https://openjdk.org/jeps/469)JDK 23
+9. [JEP 489: Vector API (Ninth Incubator)](https://openjdk.org/jeps/489) JDK 24
+
+“The Vector API will incubate until necessary features of [Project Valhalla](https://openjdk.org/projects/valhalla/) become available as preview features. At that time, we will adapt the Vector API and its implementation to use them, and will promote the Vector API from incubation to preview.”
+
+[SIMD](https://en.wikipedia.org/wiki/SIMD)(Single Instruction, Multiple Data) instruction
+
+[Streaming SIMD Extensions](https://en.wikipedia.org/wiki/Streaming_SIMD_Extensions) (SSE) 
+
+[Advanced Vector Extensions](https://en.wikipedia.org/wiki/Advanced_Vector_Extensions)(AVX)
+
+[NEON](https://en.wikipedia.org/wiki/ARM_architecture#Advanced_SIMD_(Neon))
+
+## Run
+
+```sh
+mvn clean package -DskipTests && \
+java -Xmx32g \
+    --add-modules=jdk.incubator.vector \
+    -classpath /d/coding/hello-vector-api/target/classes \
+    org.feuyeux.vector.HelloVector
+```
+
+![](coding.png)
+
+```sh
+1234 ms
+1030 ms
+1055 ms
+```
+
+![](testing.gif)
--- src/main/java/org/feuyeux/vector/HelloVector.java
+++ src/main/java/org/feuyeux/vector/HelloVector.java
@@ -1,8 +1,69 @@
 package org.feuyeux.vector;
 
-public class HelloVector {
+import jdk.incubator.vector.FloatVector;
+import jdk.incubator.vector.VectorSpecies;
 
-  public static void main(String[] args) throws Exception {
-    System.out.println("Hello Vector");
+public class HelloVector {
+  static final VectorSpecies<Float> SPECIES = FloatVector.SPECIES_PREFERRED;
+
+  void scalarComputation(float[] a, float[] b, float[] c) {
+    for (int i = 0; i < a.length; i++) {
+      c[i] = (a[i] * a[i] + b[i] * b[i]) * -1.0f;
+    }
+  }
+
+  void vectorComputation(float[] a, float[] b, float[] c) {
+    int i = 0;
+    int upperBound = SPECIES.loopBound(a.length);
+    for (; i < upperBound; i += SPECIES.length()) {
+      var va = FloatVector.fromArray(SPECIES, a, i);
+      var vb = FloatVector.fromArray(SPECIES, b, i);
+      var vc = va.mul(va).add(vb.mul(vb)).neg();
+      vc.intoArray(c, i);
+    }
+    for (; i < a.length; i++) {
+      c[i] = (a[i] * a[i] + b[i] * b[i]) * -1.0f;
+    }
+  }
+
+  void vectorComputationII(float[] a, float[] b, float[] c) {
+    for (int i = 0; i < a.length; i += SPECIES.length()) {
+      var m = SPECIES.indexInRange(i, a.length);
+      var va = FloatVector.fromArray(SPECIES, a, i, m);
+      var vb = FloatVector.fromArray(SPECIES, b, i, m);
+      var vc = va.mul(va).add(vb.mul(vb)).neg();
+      vc.intoArray(c, i, m);
+    }
+  }
+
+  static int _1B = 1_000_000_000;
+  static int _2B = 2_000_000_000;
+
+  public static void main(String[] args) {
+    HelloVector hv = new HelloVector();
+    int size = _2B;
+    float[] a = new float[size];
+    float[] b = new float[size];
+    float[] c = new float[size];
+
+    for (int i = 0; i < size; i++) {
+      a[i] = (float) Math.random();
+      b[i] = (float) Math.random();
+    }
+
+    long startTime = System.currentTimeMillis();
+    hv.scalarComputation(a, b, c);
+    long endTime = System.currentTimeMillis();
+    System.out.println((endTime - startTime) + " ms");
+
+    startTime = System.currentTimeMillis();
+    hv.vectorComputation(a, b, c);
+    endTime = System.currentTimeMillis();
+    System.out.println((endTime - startTime) + " ms");
+
+    startTime = System.currentTimeMillis();
+    hv.vectorComputationII(a, b, c);
+    endTime = System.currentTimeMillis();
+    System.out.println((endTime - startTime) + " ms");
   }
 }
